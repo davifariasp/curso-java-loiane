cc84dec5a6554a4c5a98c4f7ce27b680f8c57336
Davi Farias <davi.informatica8@gmail.com>
2025-08-05 09:48:02 -0300

    feat: primeria aula sobre trheads

---
+++ src/main/java/org/example/intermediario/threads/Main.java
@@ -0,0 +1,8 @@
+package org.example.intermediario.threads;
+
+public class Main {
+    public static void main(String[] args) {
+        MinhaThread thread1 = new MinhaThread("Thread #1", 1000);
+        MinhaThread thread2 = new MinhaThread("Thread #2", 500);
+    }
+}
---
+++ src/main/java/org/example/intermediario/threads/MinhaThread.java
@@ -0,0 +1,29 @@
+package org.example.intermediario.threads;
+
+public class MinhaThread extends Thread{
+
+    private String nome;
+    private Integer tempo;
+
+    public MinhaThread(String nome, Integer tempo) {
+        this.nome = nome;
+        this.tempo = tempo;
+        start();
+    }
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(nome + " contador " + i);
+            try {
+                sleep(tempo);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        System.out.println(nome + " terminou a execuçao.");
+    }
+
+}
