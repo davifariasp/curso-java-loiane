b16ab15c0d6eebdb7e639893c227ac0dac7c2f33
Davi Farias <davi.informatica8@gmail.com>
2025-08-05 09:55:57 -0300

    feat: aula sobre runnable

---
+++ src/main/java/org/example/intermediario/threads/MinhaThreadRunnable.java
@@ -0,0 +1,29 @@
+package org.example.intermediario.threads;
+
+public class MinhaThreadRunnable implements Runnable {
+
+    private String nome;
+    private Integer tempo;
+
+    public MinhaThreadRunnable(String nome, Integer tempo) {
+        this.nome = nome;
+        this.tempo = tempo;
+        Thread thread1 = new Thread(this);
+        thread1.start();
+    }
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(nome + " contador " + i);
+            try {
+                Thread.sleep(tempo);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        System.out.println(nome + " terminou a execuçao.");
+    }
+}
