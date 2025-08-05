cc34e833f59e1d25645155a053422fc63fd77e5e
Davi Farias <davi.informatica8@gmail.com>
2025-08-05 09:33:28 -0300

    feat: aula sobre garbage collector

---
+++ src/main/java/org/example/intermediario/garbagecollector/Main.java
@@ -0,0 +1,35 @@
+package org.example.intermediario.garbagecollector;
+
+public class Main {
+
+    public static void obterMemoriaUsada() {
+
+        Runtime runtime = Runtime.getRuntime();
+
+        long memoriaUsada = (runtime.totalMemory() - runtime.freeMemory())/ (1024 * 1024);
+
+        System.out.println("Memória usada: " + memoriaUsada + " MB");
+    }
+
+    public static void main(String[] args) {
+        Contato[] contatos = new Contato[10000];
+        Contato contato;
+
+        for(int i = 0; i < contatos.length; i++) {
+            contato = new Contato("Contato " + i, "123456789" + i, "contato" + i + "@email.com");
+            contatos[i] = contato;
+        }
+
+        obterMemoriaUsada();
+
+        System.out.println("Contatos criados com sucesso!");
+
+        contatos = null; // Liberando a referência ao array de contatos
+
+        System.gc(); // Solicita a execução do Garbage Collector
+
+        System.out.println("Contatos removidos com sucesso!");
+
+        obterMemoriaUsada();
+    }
+}
---
+++ src/main/java/org/example/intermediario/garbagecollector/README.md
@@ -0,0 +1,16 @@
+# Compile
+
+javac org/example/intermediario/garbagecollector/*.java 
+
+# Execute
+
+java -Xmx128m -verbose:gc org.example.intermediario.garbagecollector.Main
+
+### Vai retornar algo tipo 
+
+```[0.002s][info][gc] Using G1
+Memória usada: 4 MB
+Contatos criados com sucesso!
+[0,036s][info][gc] GC(0) Pause Full (System.gc()) 4M->1M(14M) 2,898ms
+Contatos removidos com sucesso!
+Memória usada: 2 MB```
